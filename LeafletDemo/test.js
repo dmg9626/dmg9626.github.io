b3e680f415d27f2f48076e5d5b7d3299dd1604f9
Drew Graham <Drew Graham>
2019-02-28 13:37:14 -0500

    restored highlight-on-hover for desktop, removed strange hover effects on mobile

--- LeafletDemo/test.js
+++ LeafletDemo/test.js
@@ -1,3 +1,30 @@
+var selectedStyle = {
+    weight: 5,
+    color: '#666',
+    dashArray: '',
+    fillOpacity: 0.7
+};
+
+var highlightedStyle = {
+    weight: 3,
+    color: '#666',
+    dashArray: '',
+    fillOpacity: 0.7
+};
+
+// sets base style for all regions on map
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+// Used to set state color based on population
 function getColor(d) {
     return d > 1000 ? '#800026' :
         d > 500  ? '#BD0026' :
@@ -10,29 +37,46 @@ function getColor(d) {
 }
 
 function onEachFeature(feature, layer) {
-    layer.on({
-        click: highlightFeature,
-        // mouseout: resetHighlight,
-        // click: zoomToFeature
-    });
+    // Mobile doesn't work well with mouseover/mouseout
+    if(isMobile()) {
+        layer.on({
+            click: selectFeature,
+        });
+    }
+
+    // Default desktop events
+    else {
+        layer.on({
+            click: selectFeature,
+            mouseover: highlightFeature,
+            mouseout: mouseout,
+        });
+    }
 }
 
-function style(feature) {
-    return {
-        fillColor: getColor(feature.properties.density),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
-    };
-}
 
 
-var currentSelectedState;
+
+
+// Called on state hover
 function highlightFeature(e) {
+    console.log("highlightFeature()");
+
     var layer = e.target;
 
+    if(!compareStates(e, currentSelectedState)) {
+        layer.setStyle(highlightedStyle);
+
+    }
+}
+
+var currentSelectedState;
+
+// Called when state is clicked
+function selectFeature(e) {
+    console.log("selectFeature()");
+
+    var layer = e.target;
 
     console.log("selected state " + layer.feature.properties.name);
 
@@ -42,12 +86,7 @@ function highlightFeature(e) {
     }
     currentSelectedState = e;
 
-    layer.setStyle({
-        weight: 5,
-        color: '#666',
-        dashArray: '',
-        fillOpacity: 0.7
-    });
+    layer.setStyle(selectedStyle);
 
     if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
         layer.bringToFront();
@@ -56,12 +95,36 @@ function highlightFeature(e) {
     info.update(layer.feature.properties);
 }
 
-function resetHighlight(e) {
-    geojson.resetStyle(e.target);
 
-    info.update();
+function compareStates(state, otherState) {
+    if(state != null && otherState != null) {
+        console.log("comparing states " + state.target.feature.properties.name + ", " + otherState.target.feature.properties.name);
+        return state.target == otherState.target;
+    }
+}
+
+function mouseout(e) {
+    console.log("mosueout()");
+    if(compareStates(e, currentSelectedState)) {
+        console.log("don't deselect!!");
+        return;
+    }
+    resetHighlight(e);
+}
+
+function resetHighlight(e) {
+    console.log("resetHighlight() on " + e.target.feature.properties.name);
+
+    // Remove stroke
+    geojson.resetStyle(e.target);
+    // info.update();
 }
 
 function zoomToFeature(e) {
     map.fitBounds(e.target.getBounds());
+}
+
+// returns true if user is on mobile, false otherwise
+function isMobile() {
+    return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 }
